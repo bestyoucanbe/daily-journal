3a740f90162fb61c2b7a80813e08e0da04a4b137
Joy Ittycheriah <bestyoucanbe@yahoo.com>
2019-06-06 20:10:31 -0500

    Completed Daily Journal 2--Added Journal entries

---
+++ src/scripts/journal.js
@@ -0,0 +1,28 @@
+//Daily Journal 2 From Here to -------------
+const objectsJournalEntry = [
+  
+  {Date_of_Entry:  "06/01/2019",
+  Concepts_Covered: "Using Git and Github",
+  Journal_Entry:  "Man, who thought of this stuff?  But it sure is cool to collaborate and work together on stuff!",
+  Mood_for_the_Day:  "Happy"},
+  
+  
+  {Date_of_Entry:  "06/06/2019",
+  Concepts_Covered: "Fat Arrow Functions, Event Listeners, API",
+  Journal_Entry:  "It was a long day but a good day!  We covered a lot of ground in a short time.",
+  Mood_for_the_Day:  "Happy"},
+
+  {Date_of_Entry:  "06/05/2019",
+  Concepts_Covered: "CRUD Method of Managing Data, Postman",
+  Journal_Entry:  "Learned how to GET and POST to the JSON-Server!  What fun is around the corner, I wonder!",
+  Mood_for_the_Day:  "Happy"}
+]
+
+const ArrListofJournalEntries = [];
+
+ArrListofJournalEntries.push(objectsJournalEntry[0]);//Pushing each item at a time into the Array!
+ArrListofJournalEntries.push(objectsJournalEntry[1]);
+ArrListofJournalEntries.push(objectsJournalEntry[2]);
+
+console.log(ArrListofJournalEntries);
+//Daily Journal 2 To Here--------------
